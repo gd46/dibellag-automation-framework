a81fa2b34f4fa89025c98f62a9375cd7cb7f98b9
DiBella, Giuseppe <Giuseppe.DiBella@ADP.com>
2017-09-01 07:11:11 -0400

    Fix file path issue using path.resolve

--- test/lib/page-object-loader.js
+++ test/lib/page-object-loader.js
@@ -1,41 +1,6 @@
-// function PageObjectsLoader() {
-  
-//   let glob = require("glob");
-//   let _ = require('lodash');
-
-//   glob("**/*.page.js", {
-//     nonull: true
-//   }, (er, filePaths) => {
-//     let pages = {};
-//     filePaths.forEach((filePath) => {
-//       let fileName = filePath.split(/(\\|\/)/g).pop(); // extract file name with extension
-//       let pageName = _.camelCase(fileName.split('.', 1)[0]); // create the file name pieces and take the first which is the name and format it
-//       let pageObjectName = pageName.charAt(0).toUpperCase() + pageName.slice(1); // make the file name match the name of the page object function or class
-//       pages[pageObjectName] = require(`./${filePath}`);
-//     });
-//     console.log('pages', pages);
-//     return pages;
-//   });
-// }
-// module.exports = PageObjectsLoader;
-
-// let glob = require("glob");
-// let _ = require('lodash');
-// let pages = {};
-//   glob("**/*.page.js", {
-//     nonull: true
-//   }, (er, filePaths) => {
-//     filePaths.forEach((filePath) => {
-//       let fileName = filePath.split(/(\\|\/)/g).pop(); // extract file name with extension
-//       let pageName = _.camelCase(fileName.split('.', 1)[0]); // create the file name pieces and take the first which is the name and format it
-//       let pageObjectName = pageName.charAt(0).toUpperCase() + pageName.slice(1); // make the file name match the name of the page object function or class
-//       pages[pageObjectName] = require(`../../${filePath}`);
-//     });
-//     console.log('pages', pages);
-//   });
-
 let glob = require("glob");
 let _ = require('lodash');
+let path = require('path');
 let pages = {};
 
   let filePaths = glob.sync("**/*.page.js", {
@@ -46,7 +11,7 @@ let pages = {};
       let fileName = filePath.split(/(\\|\/)/g).pop(); // extract file name with extension
       let pageName = _.camelCase(fileName.split('.', 1)[0]); // create the file name pieces and take the first which is the name and format it
       let pageObjectName = pageName.charAt(0).toUpperCase() + pageName.slice(1); // make the file name match the name of the page object function or class
-      pages[pageObjectName] = require(`../../${filePath}`);
+      pages[pageObjectName] = require(path.resolve(`${filePath}`));
     });
     console.log('pages', pages);
 
